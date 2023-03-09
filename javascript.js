9ad1eeef0a8b2c4772cf9b11377db4a2725a4e30
t3rv3 <113163674+t3rv3@users.noreply.github.com>
2023-03-09 13:42:38 +0200

    javascript

---
+++ javascript.js
@@ -0,0 +1,8 @@
+var me ="Ville"
+var harrastus ="koiran vienti"
+var opetus ="amiksessa" 
+var hometown ="Lohja"
+
+var whoami ="Olen "+ me +", harrastukseni on "+ harrastus +". Opiskelen "+ opetus +". Kotikaupunkini on "+ hometown +".";
+
+alert(whoami)
